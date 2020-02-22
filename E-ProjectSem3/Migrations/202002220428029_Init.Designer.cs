dcb1091dc1d377afad6f6367c83909691389f900
My PC <tvngiagia@gmail.com>
2020-02-22 11:31:48 +0700

    Fix type approveId

--- E-ProjectSem3/Migrations/202002220428029_Init.Designer.cs
+++ E-ProjectSem3/Migrations/202002220428029_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace E_ProjectSem3.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class time1 : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(time1));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "202002171756089_time1"; }
+            get { return "202002220428029_Init"; }
         }
         
         string IMigrationMetadata.Source
