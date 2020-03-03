252442507d440ec8efec05b8fdafa16e484d98fc
My PC <tvngiagia@gmail.com>
2020-03-03 22:24:58 +0700

    Add contact

---
+++ E-ProjectSem3/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_ProjectSem3.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
